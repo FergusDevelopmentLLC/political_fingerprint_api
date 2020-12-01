4c44b27e165933165354ba393fdcc857e6ae911b
Will Carter <will.carter@fergusllc.com>
2020-12-01 13:49:08 -0700

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -20,19 +20,17 @@ Rails.application.routes.draw do
 
   get '/', to: 'application#welcome'
 
-  get '/test_results_averaged', to: 'test_results#averaged_by_county'
-
   get 'questions/by_version/:version', to: 'questions#by_version'
 
   get 'test_results_fake/:limit', to: 'test_results#fake'
 
+  get '/test_results_averaged', to: 'test_results#averaged_by_county'
+  
   get 'test_result_ideology/:economic/:diplomatic/:civil/:societal', to: 'test_results#get_ideology_matches', constraints: { 
     :economic => /[^\/]+/, 
     :diplomatic => /[^\/]+/,
     :civil => /[^\/]+/,
     :societal => /[^\/]+/,
   }
-
-  patch 'test_results_check', to: 'test_results#test_results_check'
 
 end
